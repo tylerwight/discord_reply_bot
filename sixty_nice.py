dae08ffd03a4465fe3584d72b8e381a33654f9de
user <tylermwight@gmail.com>
2020-09-19 22:09:49 +0000

    comments and remove debug

--- sixty_nice.py
+++ sixty_nice.py
@@ -9,18 +9,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
+# print all servers bot is connected to on startup, just for reference
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     for guild in client.guilds:
         print(f'{guild}')
+
+
 @client.event
 async def on_message(message):
-    channel = client.get_channel(message.channel.id)
-    print (f'{channel}, {message.channel}')
+    channel = client.get_channel(message.channel.id) # get channel id from message and use it to create channel object
+#    print (f'{channel}, {message.channel}')  # print some variables for debug
     textSearch = "69"
-    print( f'{message.content}\n')
-    if message.content.find(textSearch) != -1:
+    if message.content.find(textSearch) != -1: # serach message for '69' anywhere
+        print( f'{message.content}\n') # print the mesage that triggered our "Nice."
         await channel.send('Nice.')
 
 client.run(TOKEN)
